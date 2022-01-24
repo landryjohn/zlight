cf8378441a49f510381dcaed1f5b234babc584a3
landryjohn <landryjohnmeli@gmail.com>
2022-01-25 00:08:26 +0100

    Add utilities : Help message

--- utils.py
+++ utils.py
@@ -12,3 +12,35 @@ CITIES = {
     'DOUALA' : 'X-22',
     'YAOUNDE' : 'X-1'
 }
+
+MESSAGE = """
+Bonjour, je suis zlight-bot (by landryjohn -> </lnjohn>)\n
+
+Je suis l'activité d'ENEO en temps réel et je te donne\n 
+le programme prévisionnel de coupure en raison des
+travaux de maintenance sur le réseau élétrique.
+
+Comment ca marche : \n
+--> /coupures [region ou ville]\n
+    [region] : C'est la region du cameroun dans laquelle tu te trouves, 
+    il faudra préciser comme ci-dessous. ce paramètre est optionnel
+    Exemples :\n
+    /coupures centre\n
+    /coupures littoral\n
+--> /aide : pour avoir de l'aide\n
+--> /auteur : pour avoir les informations sur mon concepteur\n
+
+Auteur : Landry John, landryjohnmeli@gmail.com\n
+"""
+
+HELP = """
+Comment ca marche : \n
+--> /coupures [region ou ville]\n
+    [region] : C'est la region du cameroun dans laquelle tu te trouves, 
+    il faudra préciser comme ci-dessous. ce paramètre est optionnel
+    Exemples :\n
+    /coupures centre\n
+    /coupures littoral\n
+--> /aide : pour avoir de l'aide\n
+--> /auteur : pour avoir les informations sur mon concepteur\n
+"""
