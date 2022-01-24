4fe6e61e8f85f82843e09ad54fa52d221b95d3e0
landryjohn <landryjohnmeli@gmail.com>
2022-01-24 23:16:27 +0100

    Moved CITIES to utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+CITIES = {
+    'ADAMAOUA' : 1, 
+    'CENTRE' : 2 , 
+    'EST' : 3,
+    'EXTREME-NORD' : 4,
+    'LITTORAL' : 5,
+    'OUEST' : 6,
+    'NORD' : 7,
+    'NORD-OUEST' : 8,
+    'SUD' : 9,
+    'SUD-OUEST' : 10,
+    'DOUALA' : 'X-22',
+    'YAOUNDE' : 'X-1'
+}
