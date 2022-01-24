2aecd59fe82c0d55f2cf48f6c00f275317acbaf8
landryjohn <landryjohnmeli@gmail.com>
2022-01-24 23:34:54 +0100

    function to get all schedule cuts by one valid region

--- app.py
+++ app.py
@@ -16,17 +16,30 @@ logger = logging.getLogger(__name__)
 
 def cuts() -> object:
     req = '/coupure EST'
-    #  Verify if only one city is requested, multiple cities if for the next version of the bot
+    #  Verify if only one valid city is requested, multiple cities if for the next version of the bot
     city = [city for city in req.split() if city.upper() in CITIES.keys()]
     if any(city) and len(city) < 2 :
         try :
+            # Send request to the API
             resp = requests.post(url="https://alert.eneo.cm/ajaxOutage.php", data={'region':CITIES[city[0]]}).json()
-            return resp
+            formated_resp = ''
+            for cut in resp['data']:
+                formated_resp += '=============PROGRAMME DE COUPURE===============\n'
+                formated_resp += f"REGION: {cut['region']}\n"
+                formated_resp += f"VILLE: {cut['ville']}\n"
+                formated_resp += f"DATE : {cut['prog_date']} | HEURE DEBUT : {cut['prog_heure_debut']} | HEURE FIN : {cut['prog_heure_fin']}\n"
+                formated_resp += f"DETAILS : {cut['observations']}\n"  
+                formated_resp += '================================================\n\n'
+            return formated_resp
         except Exception as error :
             print("An error occured ", error) 
     else : 
         return {'status': False, 'message': "Vous devez fournir une seule ville valide"}
 
-
 if __name__ == "__main__" :
-    r = cuts() 
+    resp = cuts()
+    if any(resp) :   
+        print(resp)
+    else :
+        print("AUCUN PROGRAMME DE COUPURE DANS CETTE ZONE") 
+
